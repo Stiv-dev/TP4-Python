4406a3331773ddb33e128671b1ab293857948fb9
YAGAPIN <yagapin@et.esiea.fr>
2022-04-01 10:41:26 +0200

    3eme commit

--- Tp4.py
+++ Tp4.py
@@ -2,11 +2,28 @@ import discord
 from discord import Client
 
 client = discord.Client()
-client.run("OTU5MzQ4MTE2OTA4Mjc3ODIw.Ykakjg.apXORzqsql1PR-Qm1wp-3CtjuE4")
 
-class MyBot(Client):
+@client.event
+async def on_ready():
+    print("Le bot est pret.")
+
+@client.event
+async def on_message(message):
+    print(message.content)
+client.run("OTU5MzQ4MTE2OTA4Mjc3ODIw.Ykakjg.88QgJ_0F2QEWXwGASjSvdcuEbw0")
+
+"""@client.event
+async def on_message(message):
+    pass
+
+@client.event
+async def on_message(message):
+    if message.content == "Ping":
+        await message.channel.send("Pong")"""
+
+"""class MyBot(Client):
     def __init__(self):
         super().__init__()
 
     async def on_ready(self):
-        self.log.infolog(f"{self.user} has connected to Discord!")
+        self.log.infolog(f"{self.user} has connected to Discord!")"""
