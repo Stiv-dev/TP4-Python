c2b69fb330b8412b5bfa94901108d792be46c325
YAGAPIN <yagapin@et.esiea.fr>
2022-04-01 10:03:01 +0200

    2eme commit

--- Tp4.py
+++ Tp4.py
@@ -1,5 +1,8 @@
+import discord
 from discord import Client
 
+client = discord.Client()
+client.run("OTU5MzQ4MTE2OTA4Mjc3ODIw.Ykakjg.apXORzqsql1PR-Qm1wp-3CtjuE4")
 
 class MyBot(Client):
     def __init__(self):
