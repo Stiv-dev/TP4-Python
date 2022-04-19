9b66bb93963636f11663ef310053f18df781b0dd
YAGAPIN <yagapin@et.esiea.fr>
2022-04-19 22:12:51 +0200

    4ème commit

--- Tp4.py
+++ Tp4.py
@@ -1,26 +1,68 @@
 import discord
 from discord import Client
 
-client = discord.Client()
+#pr config le token
+from dotenv import load_dotenv
+load_dotenv(dotenv_path="config")
+import os
+
+default_intents = discord.Intents.default()
+default_intents.members = True
+client = discord.Client(intents=default_intents)
+
+
+"client = discord.Client()"
 
 @client.event
 async def on_ready():
     print("Le bot est pret.")
 
 @client.event
+async def on_member_join(member):
+    general_channel: discord.TextChannel = client.get_channel(959382781996187700)
+    await general_channel.send(content=f"Bienvenue sur le serveur {member.display_name} !")
+
+@client.event
+async def on_member_remove(member):
+    general_channel: discord.TextChannel = client.get_channel(959382781996187700)
+    await general_channel.send(content=f"Va te faire {member.display_name} !")
+
+@client.event 
 async def on_message(message):
-    print(message.content)
-client.run("OTU5MzQ4MTE2OTA4Mjc3ODIw.Ykakjg.88QgJ_0F2QEWXwGASjSvdcuEbw0")
+    if message.content.startswith("!je_suis_venere"):
+        number = int(message.content.split()[1])
+        messages = await message.channel.history(limit=number +1).flatten()
+
+        for each_message in messages:
+            await each_message.delete()
+            
+
+"""
+@client.event
+async def on_message(message):
+    print(message.content)"""
+
+
+@client.event
+async def on_message(message):
+    if message.content.lower() == "ping":
+        await message.channel.send("pong")
+        #await message.channel.send("pong", delete_after=5)
+
+client.run(os.getenv("TOKEN"))
+
+
 
 """@client.event
 async def on_message(message):
     pass
-
+"""
+"""
 @client.event
 async def on_message(message):
     if message.content == "Ping":
-        await message.channel.send("Pong")"""
-
+        await message.channel.send("Pong")
+"""
 """class MyBot(Client):
     def __init__(self):
         super().__init__()
